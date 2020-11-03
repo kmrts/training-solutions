94c7d9d18d78ac5a1a1db8db4aefd50753ecfe2c
kmz <maartsee@gmail.com>
2020-11-03 19:05:50 +0100

    Product

--- src/main/java/classstructureconstructors/Store.java
+++ src/main/java/classstructureconstructors/Store.java
@@ -2,12 +2,21 @@ package classstructureconstructors;
 
 public class Store {
     private String product;
-    public int stock;
+    private int stock;
 
     public Store(String product) {
         this.product = product;
         this.stock= 0;
     }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
     public void store(int store){
         stock= stock + store;
     }
--- src/main/java/classstructureconstructors/StoreMain.java
+++ src/main/java/classstructureconstructors/StoreMain.java
@@ -5,17 +5,17 @@ public class StoreMain {
         Store store1= new Store("coffee");
         Store store2= new Store("cocoa");
 
-        System.out.println("Nyitó: " +store1.stock);
+        System.out.println("Nyitó: " +store1.getStock());
         store1.store(200);
-        System.out.println(store1.stock);
+        System.out.println(store1.getStock());
         store1.dispatch(100);
-        System.out.println(store1.stock);
+        System.out.println(store1.getStock());
 
-        System.out.println("Nyitó: " +store2.stock);
+        System.out.println("Nyitó: " +store2.getStock());
         store2.store(350);
-        System.out.println(store2.stock);
+        System.out.println(store2.getStock());
         store2.dispatch(125);
-        System.out.println(store2.stock);
+        System.out.println(store2.getStock());
 
     }
 }
