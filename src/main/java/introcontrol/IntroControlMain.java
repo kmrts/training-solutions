c9dd8982ef0b26f8837928674eda473fce920118
kmz <maartsee@gmail.com>
2020-11-04 23:16:13 +0100

    i c

---
+++ src/main/java/introcontrol/IntroControlMain.java
@@ -0,0 +1,13 @@
+package introcontrol;
+
+public class IntroControlMain {
+    public static void main(String[] args) {
+        IntroControl ic= new IntroControl();
+
+        System.out.println(ic.substractTenIfGreaterThanTen(12));
+
+        System.out.println(ic.greetingToJoe("Joe M"));
+
+        System.out.println(ic.describeNumber(25));
+    }
+}
