7df3cf4bfc17f2712de95240ad94f77434806c2d
kmz <maartsee@gmail.com>
2020-11-07 00:22:55 +0100

    game first

---
+++ src/main/java/random/Warrior.java
@@ -0,0 +1,16 @@
+package random;
+
+public class Warrior {
+    private String name;
+    private int stamina;
+    private double skill;
+
+    public Warrior(String name, Point point) {
+        this.name = name;
+        ///
+    }
+   // public Point getPosition(){
+
+   // }
+
+}
