3ffcbf2a586f48e55cf8f0a5426a3e46352204d0
kmz <maartsee@gmail.com>
2020-11-07 11:52:23 +0100

    2nd

--- src/main/java/math/RoundingAnomaly.java
+++ src/main/java/math/RoundingAnomaly.java
@@ -5,25 +5,58 @@ import java.util.Random;
 public class RoundingAnomaly {
 
     Random rnd = new Random();
+//    private double[] randoms= new double[size];
 
     public double[] randomNumbers(int size, double max, int scale){
-        double[] randoms= new double[size];
+       double[] randoms= new double[size];
         for (int i=0; i<size; i++) {
             randoms[i]=(Math.round(rnd.nextDouble() *max* Math.pow(10, scale) )) /Math.pow(10, scale);
         }
         return randoms;
     }
+    public double roundAfterSum(double[] numbers){
+        double sum= 0;
+        for (double num: numbers) {
+            sum+= num;
+        }
+        return Math.round(sum);
+    }
+    public double sumAfterRound(double[] numbers){
+        double sum= 0;
+        for (double num: numbers) {
+            sum+= Math.round(num);
+        }
+        return sum;
 
+    }
+    double difference(int size, double max, int scale){
+        double[] nums= randomNumbers(size, max, scale);
+        double afterSum= roundAfterSum(nums);
+      //  System.out.println(afterSum);
+        double afterRound= sumAfterRound(nums);
+//        System.out.println(afterRound);
+//        System.out.println(Math.abs(afterSum- afterRound));
+        return Math.abs(afterSum- afterRound);
 
+    }
     public static void main(String[] args) {
 
         RoundingAnomaly ra= new RoundingAnomaly();
 
-        //System.out.println(ra.randomNumbers(10, 1000000, 5).toString() );
-        //double[] tomb = new ra.randomNumbers(10, 1000000, 5);
-        for(double num: ra.randomNumbers(20, 1000000, 5)){
-            System.out.println(num);
+//        for(double num: ra.randomNumbers(20, 1000000, 5)){
+//            System.out.println(num);
+//        }
+//        ra.difference(20, 1_000_000, 5);
+
+        int count = 100;
+        double sum = 0;
+        for (int i = 0; i < count; i++) {
+            double diff = ra.difference(1000, 1_000_000, 5);
+            sum += diff;
+            System.out.println("Különbség: " + diff);
         }
+        System.out.println("A különbségek átlaga:"+ sum / count);
+
 
 
     }
