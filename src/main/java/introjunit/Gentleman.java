84d4920f50d49e9cf4ce47cb320d3d98ed07941c
Librarian <maartsee@gmail.com>
2020-11-11 14:24:13 +0000

    1st

---
+++ src/main/java/introjunit/Gentleman.java
@@ -0,0 +1,10 @@
+package introjunit;
+
+public class Gentleman {
+    public static void main(String[] args) {
+
+        public String sayHello(String name){
+            return "Hello "+ name;
+        }
+    }
+}
