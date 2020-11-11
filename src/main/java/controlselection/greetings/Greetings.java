8cf8ab631547807472fcc4532152eb9e4b3b5ec8
kmz <maartsee@gmail.com>
2020-11-11 22:46:20 +0100

    greet 1

---
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -0,0 +1,11 @@
+package controlselection.greetings;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Greetings {
+    public String Greeting(int hour, int minute){
+        LocalTime greeT=LocalTime.of(hour, minute);
+        case greeT{
+    }
+}
