8a803b7b9e7c1b5668ce26e72e84aa48b13c9c45
Librarian <maartsee@gmail.com>
2020-11-12 10:36:05 +0000

    greet

--- src/main/java/controlselection/greetings/Greetings.java
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -4,8 +4,31 @@ import java.time.LocalTime;
 import java.util.Date;
 
 public class Greetings {
+
     public String Greeting(int hour, int minute){
-        LocalTime greeT=LocalTime.of(hour, minute);
-        case greeT{
+        LocalTime greeTime=LocalTime.of(hour, minute);
+        String greet= "";
+        if(greeTime.getHour()>=20  || greeTime.getHour()<5){
+            greet= "Jó éjt!";
+        }else if(greeTime.getHour()>=18 && greeTime.getMinute()>=30){
+            return "Jó estét!";
+        }else if(greeTime.getHour()>=9){
+            return "Jó napot!";
+        }else if(greeTime.getHour()>=5){
+            return "Jó reggelt!";
+        }
+        return greet;
+    }
+
+
+    public static void main(String[] args) {
+        Greetings gr= new Greetings();
+
+        System.out.println(gr.Greeting( LocalTime.now().getHour(),LocalTime.now().getMinute()));
+
+        System.out.println((gr.Greeting(5,0)));
+        System.out.println((gr.Greeting(18,29)));
+        System.out.println((gr.Greeting(18,30)));
     }
 }
+
