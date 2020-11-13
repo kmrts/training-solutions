ea4e956ae98d032ee2f76a2835553802f39056be
kmz <maartsee@gmail.com>
2020-11-13 18:08:20 +0100

    @Test és assertEquals jav

--- src/test/java/introjunit/GentlemanTest.java
+++ src/test/java/introjunit/GentlemanTest.java
@@ -1,8 +1,12 @@
 package introjunit;
 
-import org.junit.Test;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GentlemanTest {
 
