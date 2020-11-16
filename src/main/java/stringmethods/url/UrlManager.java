877008a3fadfbd45184ea1c687a77c044c218939
Librarian <maartsee@gmail.com>
2020-11-16 16:03:24 +0000

    url 1st

---
+++ src/main/java/stringmethods/url/UrlManager.java
@@ -0,0 +1,37 @@
+package stringmethods.url;
+
+public class UrlManager {
+    private String protocol;
+    private Integer port;
+    private String host;
+    private String path;
+    private String query;
+
+
+
+    public UrlManager(String url) {
+
+        public String getProtocol(){
+            if(!url.contains("://")){
+                throw new IllegalArgumentException("Illegal argument!");
+            }
+            return url.substring(0, url.indexOf("://"));
+        }
+        public Integer getPort() {
+
+            return port;
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public String getQuery() {
+            return query;
+        }
+    }
+}
