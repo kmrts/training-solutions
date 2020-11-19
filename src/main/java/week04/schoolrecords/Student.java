52d3eeb77040ca2ac41b245faf980d49f1ab069a
Librarian <maartsee@gmail.com>
2020-11-19 15:51:37 +0000

    1st

---
+++ src/main/java/week04/schoolrecords/Student.java
@@ -0,0 +1,46 @@
+package week04.schoolrecords;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+    private List<Mark> marks= new ArrayList<>();
+    private String name;
+
+    public Student(String name) {
+//        this.marks = marks;
+        this.name = name;
+    }
+
+    public List<Mark> getMarks() {
+        return marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "marks=" + marks +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+//    public void grading(Mark mark){
+//
+//    } // érdemjegy rögzítése
+//    public double calculateAverage(){
+//
+//    } // teljes átlag számolása
+//    public double calculateSubjectAverage(Subject subject){
+//
+//    } // tantárgyhoz tartozó átlag számítása
+//
+//    private boolean isEmpty(String str){
+//
+//    }
+
+}
