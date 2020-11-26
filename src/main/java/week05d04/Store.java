330d2a83da7ad26fb10721275d5686f8fd9fab15
kmz <maartsee@gmail.com>
2020-11-26 22:35:42 +0100

    store

--- src/main/java/week05d04/Store.java
+++ src/main/java/week05d04/Store.java
@@ -1,14 +1,21 @@
 package week05d04;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Store {
-    //Készíts egy week05d04.Store osztályt, melyben van egy Productokat tároló lista. Legyen benne egy addProduct(Product) metódus.
-    //Bónusz: Legyen benne egy getNumberOfExpired() metódus ami visszaadja a lejárt termékek számát. Használd a LocalDate.now() és az isBefore() metódusokat.
+    //Készíts egy week05d04.Store osztályt, melyben van egy Productokat tároló lista.
+    // Legyen benne egy addProduct(Product) metódus.
+    //Bónusz: Legyen benne egy getNumberOfExpired() metódus ami visszaadja a lejárt termékek számát.
+    // Használd a LocalDate.now() és az isBefore() metódusokat.
     //Tesztelj!
     private List<Product> products= new ArrayList<>();
 
+//    public Store() {
+//
+//    }
     public Store(List<Product> products) {
         this.products = products;
     }
@@ -20,4 +27,14 @@ public class Store {
     public void addProduct(Product productToAdd){
         products.add(productToAdd);
     }
+    public int getNumberOfExpired(){
+        int count=0;
+        for(Product pItem: products){
+            if(pItem.getDate().isBefore(LocalDate.now())){
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
--- src/test/java/week05d04/StoreTest.java
+++ src/test/java/week05d04/StoreTest.java
@@ -2,6 +2,7 @@ package week05d04;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,21 +16,26 @@ public class StoreTest {
     @Test
     public void testStore(){
 
-
         assertEquals("asztal", st.getProducts().get(1).getName());
 
     }
-
     @Test
-    public void testAdd(){
-        Store st2= new Store(Arrays.asList(new Product("pad", 2020, 12, 20)) );
+    public void testAddandExpired(){
+        //Tömbökből is készíthetünk listát az Arrays.asList() metódussal azzal a megkötéssel,
+        // hogy elemeket utólagosan sem hozzáadni, sem törölni nem tudunk.
 
+        List<Product> p1= new ArrayList<>();
+        p1.add(new Product("pad", 2020, 12, 20));
+        Store s= new Store(p1);
 
-        st2.addProduct(new Product("deszka", 2021, 01,20));
-        System.out.println(st2.getProducts().get(2).getDate());
+        Product p= new Product("másvalami", 2020, 10, 9);
+        s.addProduct(p);
 
-//        assertEquals("2021-01-20", st.getProducts().get(2).getDate());
+        assertEquals(10, s.getProducts().get(1).getDate().getMonthValue());
 
+        assertEquals(1, s.getNumberOfExpired());
+        s.addProduct(new Product("harmadik", 2020, 11, 25));
+        assertEquals(2, s.getNumberOfExpired());
     }
 
 
