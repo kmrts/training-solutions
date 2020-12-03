1c26757bb20ea2e918e9db3cd4a170a5ace74eae
kmz <maartsee@gmail.com>
2020-12-03 22:54:49 +0100

    jav

--- src/main/java/defaultconstructor/date/SimpleDateFormatter.java
+++ src/main/java/defaultconstructor/date/SimpleDateFormatter.java
@@ -6,15 +6,16 @@ import java.time.format.DateTimeFormatter;
 public class SimpleDateFormatter {
     // a másik az alapértelmezett országkódot használja.
     // Ezt az osztály paraméter nélküli konstruktora állítja be.
-
+    private CountryCode defaultCountry; ////
 
     public SimpleDateFormatter() {
-
+        this.defaultCountry = CountryCode.HU;  ////
     }
 
     public String formatDateString(SimpleDate simpleDate){
-        LocalDate ld= LocalDate.of(simpleDate.getYear(), simpleDate.getMonth(), simpleDate.getDay());
-        return ld.format(DateTimeFormatter.ofPattern("yyyy-M-dd"));
+//        LocalDate ld= LocalDate.of(simpleDate.getYear(), simpleDate.getMonth(), simpleDate.getDay());
+//        return ld.format(DateTimeFormatter.ofPattern("yyyy-M-dd"));
+        return formatDateStringByCountryCode(defaultCountry, simpleDate);  ////
     }
     public String formatDateStringByCountryCode(CountryCode countryCode, SimpleDate simpleDate){
         LocalDate ld= LocalDate.of(simpleDate.getYear(), simpleDate.getMonth(), simpleDate.getDay());
