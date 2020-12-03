6027db4bab84fc6046f2b91adf91dbf352a99368
kmz <maartsee@gmail.com>
2020-12-03 18:39:19 +0100

    simple date

---
+++ src/main/java/defaultconstructor/date/SimpleDate.java
@@ -0,0 +1,69 @@
+package defaultconstructor.date;
+
+public class SimpleDate {
+
+    private int year;
+    private int month;
+    private int day;
+
+    public void setDate(int year, int month, int day) {
+        if(!isCorrect(year, month, day)){
+            throw new IllegalArgumentException(String.format("One or more given parameter cannot be applied! %d, %d, %d", year, month, day));
+        }
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+    private boolean isCorrect(int year, int month, int day){
+        return year>= 1900 && month>=1 && month <= 12
+                && day<=calculateMonthLength(year, month) && day>0 ? true : false;
+    }
+    private int calculateMonthLength(int year, int month){
+        int length= 0;
+        switch(month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                length= 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                length= 30;
+                break;
+            case 2:
+                length= isLeapYear(year) ? 29 : 28;
+                break;
+        }
+        return length;
+    }
+    private boolean isLeapYear(int year){
+//        if(year%400==0){
+//            return true;
+//        }else if(year%100==0){
+//            return false;
+//        }else if(year%4==0){
+//            return true;
+//        }else{
+//            return false;
+//        }
+        return year%400==0 ? true : (year%100==0 ? false : (year%4==0 ? true : false));
+    }
+}
