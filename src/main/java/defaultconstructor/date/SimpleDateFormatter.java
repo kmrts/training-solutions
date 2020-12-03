305718f8503487e540c0b05a849c935a204b6436
kmz <maartsee@gmail.com>
2020-12-03 22:39:36 +0100

    simple d form 1st

---
+++ src/main/java/defaultconstructor/date/SimpleDateFormatter.java
@@ -0,0 +1,36 @@
+package defaultconstructor.date;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class SimpleDateFormatter {
+    // a másik az alapértelmezett országkódot használja.
+    // Ezt az osztály paraméter nélküli konstruktora állítja be.
+
+
+    public SimpleDateFormatter() {
+
+    }
+
+    public String formatDateString(SimpleDate simpleDate){
+        LocalDate ld= LocalDate.of(simpleDate.getYear(), simpleDate.getMonth(), simpleDate.getDay());
+        return ld.format(DateTimeFormatter.ofPattern("yyyy-M-dd"));
+    }
+    public String formatDateStringByCountryCode(CountryCode countryCode, SimpleDate simpleDate){
+        LocalDate ld= LocalDate.of(simpleDate.getYear(), simpleDate.getMonth(), simpleDate.getDay());
+        String pattern="";
+        switch (countryCode){
+            case EN:
+                pattern= "dd-M-yyyy";
+                break;
+            case US:
+                pattern= "M-dd-yyyy";
+                break;
+            case HU:
+                pattern= "yyyy-M-dd";
+                break;
+        }
+        return ld.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+}
