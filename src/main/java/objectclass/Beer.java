4e94ee1db5125ecdf08acfabf91ca124e88d77df
kmz <maartsee@gmail.com>
2020-12-07 23:17:50 +0100

    simplebag

---
+++ src/main/java/objectclass/Beer.java
@@ -0,0 +1,33 @@
+package objectclass;
+
+import java.util.Objects;
+
+public class Beer {
+    //Beer osztály String name és int price attribútumokkal
+    //
+    //publikus metódusok:
+    //
+    // public Beer(String name, int price)
+    // public boolean equals(Object o) // generált metódus!
+    private String name;
+    private int price;
+
+    public Beer(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Beer beer = (Beer) o;
+        return price == beer.price &&
+                Objects.equals(name, beer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
---
+++ src/main/java/objectclass/SimpleBag.java
@@ -0,0 +1,49 @@
+package objectclass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleBag {
+    //A SimpleBag osztály egy tetszőleges számú Object típusú objektumot tartalmazó adatszerkezetet
+    // reprezentál, a Book és Beer osztályok a kipróbálását segítik.
+    // A tartalmazott objektumok között lehetnek azonosak is.
+    // A SimpleBag osztály a tartalmazott objektumokat egy List<Object> items attribútumban tárolja,
+    // és saját metódusai vannak az adatszerkezet kezelésére.
+    // Az adatszerkezet bejárása a "kurzor" szemlélettel lehetséges.
+    // A kurzor a létrehozáskor az első elem előtt áll, és bármikor újra ide helyezhető
+    // a beforeFirst metódus meghívásával. Azaz a bejárás megismételhető.
+    //
+    //publikus metódusok:
+    //
+    // public SimpleBag()
+    // public void putItem(Object item)
+    // public boolean isEmpty()
+    // public int size()
+    // public void beforeFirst()
+    // public boolean hasNext()
+    // public Object next()
+    // public boolean contains(Object item)
+    // public int getCursor()
+    private List<Object> items = new ArrayList<>();
+    private int cursor= 0;
+
+    public SimpleBag(List<Object> items) { //
+        this.items = items;
+    }
+
+    public void putItem(Object item) {
+        items.add(item);
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void beforeFirst() {
+        cursor = 0;
+    }
+}
