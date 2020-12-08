84eb2ff1d0b454432fb9ce5766fdcc1bb530dfad
kmz <maartsee@gmail.com>
2020-12-08 22:29:38 +0100

    3rd

--- src/main/java/objectclass/SimpleBag.java
+++ src/main/java/objectclass/SimpleBag.java
@@ -4,46 +4,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleBag {
-    //A SimpleBag osztály egy tetszőleges számú Object típusú objektumot tartalmazó adatszerkezetet
-    // reprezentál, a Book és Beer osztályok a kipróbálását segítik.
-    // A tartalmazott objektumok között lehetnek azonosak is.
-    // A SimpleBag osztály a tartalmazott objektumokat egy List<Object> items attribútumban tárolja,
-    // és saját metódusai vannak az adatszerkezet kezelésére.
-    // Az adatszerkezet bejárása a "kurzor" szemlélettel lehetséges.
+
     // A kurzor a létrehozáskor az első elem előtt áll, és bármikor újra ide helyezhető
     // a beforeFirst metódus meghívásával. Azaz a bejárás megismételhető.
-    //
-    //publikus metódusok:
-    //
-    // public SimpleBag()
-    // public void putItem(Object item)
-    // public boolean isEmpty()
-    // public int size()
-    // public void beforeFirst()
-    // public boolean hasNext()
-    // public Object next()
-    // public boolean contains(Object item)
-    // public int getCursor()
-    private List<Object> items = new ArrayList<>();
-    private int cursor= 0;
 
-    public SimpleBag(List<Object> items) { //
-        this.items = items;
+    private List<Object> items = new ArrayList<>();
+    private int cursor= 0; //-1
+
+    public SimpleBag() { //
+        beforeFirst(); //
     }
 
     public void putItem(Object item) {
         items.add(item);
+        cursor++;
     }
 
-//    public boolean isEmpty() {
-//
-//    }
+    public boolean isEmpty() {
+        return items.size()==0;
+
+    }
 
     public int size() {
         return items.size();
     }
 
     public void beforeFirst() {
-        cursor = 0;
+        cursor = 0;  //-1
+    }
+    public boolean hasNext(){
+        return items.size()>cursor; //items.get(cursor)!= null
+    }
+    public Object next(){
+        cursor++;
+        return items.get(cursor-1);
+
+    }
+    public boolean contains(Object item){  //.contains
+//        items.contains(item);
+        for(Object obj: items){
+            if(obj.equals(item)){ //nem==
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCursor() {
+        return cursor;
     }
 }
