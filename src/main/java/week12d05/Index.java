39789a4685246a79099de0ad21e2220b8afe1959
kmz <maartsee@gmail.com>
2021-01-24 20:48:52 +0100

    ciklus

--- src/main/java/week12d05/Index.java
+++ src/main/java/week12d05/Index.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Index {
     /*
@@ -11,38 +14,39 @@ public class Index {
     Másold be a projektedbe (index.html) néven! Ez egy szöveges állomány.
     Olvasd be, és számold meg, hány sorban szerepel a koronavírus szó! (edited)
      */
-    public int countWord(String word){
-        Path file= Path.of("index.html");
-        int count= 0;
+    public int countWord(String word) {
+        Path file = Path.of("index.html");
+        int count = 0;
         try (BufferedReader bur = Files.newBufferedReader(file)) {
             String nextLine;
-            while( ( nextLine= bur.readLine() ) != null ){
-                if(nextLine.contains(word)){
+            while ((nextLine = bur.readLine()) != null) {
+                if (nextLine.contains(word)) {
                     count++;
                 }
             }
-        }catch (IOException ex){
+        } catch (IOException ex) {
             throw new IllegalStateException("can not read file", ex);
         }
         return count;
     }
 
+    public String countCycle(List<String> searchWords) {
+        StringBuilder sb = new StringBuilder();
+        for (String st : searchWords) {
+            sb.append(String.format("%s(%d) ", st, countWord(st)));
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        int i= new Index().countWord("koro"); // 29
-        System.out.println(i);
+//        int i = new Index().countWord("koro"); // 29
+//        System.out.println(i);
 
-        Index ind= new Index();
-
-        System.out.println(ind.countWord("koronavírus")); //7
-        System.out.println(ind.countWord("koronavirus")); //16
-        System.out.println(ind.countWord("vakcina")); //9
-        System.out.println(ind.countWord("szex")); // 9
-        System.out.println(ind.countWord("medve")); // 2
-        System.out.println(ind.countWord("Orbán"));// 2
-        System.out.println(ind.countWord("Merkel"));// 1
-        System.out.println(ind.countWord("Putyin"));// 1
-        System.out.println(ind.countWord("magyar")); // 18
-        System.out.println(ind.countWord("pénz")); // 2
+        Index ind = new Index();
+        List<String> list= new ArrayList<>(Arrays.asList("koro", "koronavírus", "koronavirus", "vakcina", "oltás", "szex", "medve",
+                "majom", "Orbán", "Merkel", "Putyin", "Trump", "Kamala", "Müller", "pénz", "magyar", "java", "42",
+                "kobra", "python", "anakonda","c"));
+        System.out.println(ind.countCycle(list));
 
     }
 }
