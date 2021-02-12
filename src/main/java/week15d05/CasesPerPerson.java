8d51713ffd752438daca7259189ecde5783df76f
Librarian <maartsee@gmail.com>
2021-02-12 13:01:11 +0000

    init

---
+++ src/main/java/week15d05/CasesPerPerson.java
@@ -0,0 +1,14 @@
+package week15d05;
+
+public class CasesPerPerson {
+    /*
+    Senior feladat
+Letölthetőek a COVID adatok a
+https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/data.csv
+címről.
+Összegezd országonként az esetszámokat, és add vissza azt a három
+országot, ahol az összes esetszám populációra arányosítva a legnagyobb.
+     */
+
+    //Map, orsz, lista int v double
+}
