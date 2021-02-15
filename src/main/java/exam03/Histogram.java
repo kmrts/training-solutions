85642ff465812e8261941b4d26686b90035535c5
kmz <maartsee@gmail.com>
2021-02-15 17:32:34 +0100

    1st

---
+++ src/main/java/exam03/Histogram.java
@@ -0,0 +1,56 @@
+package exam03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Histogram {
+    /*
+    Olvass be egy szöveges fájlt soronként, melyben soronkont számok vannak.
+    Majd hozz létre egy stringet, mely ugyanennyi sort tartalmaz, és annyi csillagot ír ki egymás mellé soronkont,
+    amennyi a bemeneti fájlban lévő szám.
+Azaz a bemeneti fájl:
+
+3
+4
+1
+2
+A létrehozott String:
+
+***
+****
+*
+**
+
+Figyelj, hogy az algoritmusod lehetőleg hatékony legyen!
+
+A Histogram osztályba dolgozz, tesztje HistogramTest. Minden sor után legyen sortörés, az utolsó után is.
+     */
+    public String createHistogram(BufferedReader reader) throws IOException {
+        String line;
+        String result= "";
+        StringBuilder sb= new StringBuilder();
+        while ((line = reader.readLine())  != null) {
+
+            int times= Integer.parseInt(line); //try
+//            String actual;
+            for(int i=0; i< times; i++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = Files.newBufferedReader(Path.of("histogram.txt"))) {
+
+            System.out.println(new Histogram().createHistogram(reader));
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+}
+
