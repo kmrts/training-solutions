16a7e7492575d8cd9eb9d9377dbc318b212d1b27
kmz <maartsee@gmail.com>
2021-04-02 15:23:11 +0200

    fel

--- src/main/java/week05d05/senior/Song.java
+++ src/main/java/week05d05/senior/Song.java
@@ -1,24 +1,6 @@
 package week05d05.senior;
 
 public class Song {
-    /*
-    az alábbi mezői vannak:
-
-    String name
-    long lengthInSeconds
-    String artist
-
-A Song konstruktorában ellenőrizd, hogy a name és az artist üres-e!
-Dobj kivételt, ha igen! Ellenőrizd továbbá a lengthInSeconds-ot is, hogy nagyobb legyen, mint 0!
-Készítsünk egy Playlist osztályt, melynek van egy konstruktora,
-amiben Song-ok listáját fogadja el!
-Írjunk egy metódust findByLengthGreaterThan néven,
-mely egy int-et vár paraméterként mins néven. Azoknak a Song-oknak a listáját
-adjuk vissza, melyek hosszabbak percben (!), mint a Song lengthInSeconds mezője.
-
-Bónusz feladat: Implementáljuk megfelelően a toString metódust a Song és a Playlist osztályban,
-hogy a Playliset-et kiirva a konzolra olvasható eredményt kapjunk!
-     */
 
     private String name;
     private long lengthInSeconds;
