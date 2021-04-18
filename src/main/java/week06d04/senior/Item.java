7fdddb60dc6d2adae2d6a6936b8c3cd90bd91e93
kmz <maartsee@gmail.com>
2021-04-18 16:28:47 +0200

    ideis

---
+++ src/main/java/week06d04/senior/Item.java
@@ -0,0 +1,27 @@
+package week06d04.senior;
+
+public class Item {
+    private String name;
+    private int quantity;
+
+    public Item(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
